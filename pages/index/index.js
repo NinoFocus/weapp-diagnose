5461b3f815b7be57a3c2c82e87147e5fb9567e79
Nino <nino@youzan.com>
2017-10-25 17:07:22 +0800

    添加复制信息的功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 Page({
   data: {
-    systemInfo: {},
+    systemInfo: null,
     networkType: ''
   },
 
@@ -20,9 +20,30 @@ Page({
   getNetworkType() {
     wx.getNetworkType({
       success: (res) => {
-        console.log(res)
         this.setData({
           networkType: res.networkType
+        })
+      }
+    })
+  },
+
+  handleCopy() {
+    const { networkType, systemInfo } = this.data
+    let data = []
+
+    data.push(`网络: ${networkType}`)
+    data.push(`客户端平台: ${systemInfo.platform}`)
+    data.push(`操作系统版本: ${systemInfo.system}`)
+    data.push(`微信版本号: ${systemInfo.version}`)
+    data.push(`客户端基础库版本: ${systemInfo.SDKVersion}`)
+    data.push(`手机品牌: ${systemInfo.brand}`)
+    data.push(`手机型号: ${systemInfo.model}`)
+
+    wx.setClipboardData({
+      data: data.join('\n'),
+      success: function() {
+        wx.showToast({
+          title: '复制成功'
         })
       }
     })
